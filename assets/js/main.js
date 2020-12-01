1712911a7b59feca4e645bfbd851ad5accdf68fd
Loris Matteri <loris.matteri93@gmail.com>
2020-12-01 17:50:38 +0100

    Numeri indovinati
    con includes verifico quali numeri inseriti dall'user sono presenti nell'array dei numeri random e se sono presenti li pusho in un altro array da cui prendo quanti numeri sono stati indovinati con length e faccio apparire tutto nella pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 $(function() {
+    //Alert con istruzioni
+    alert('Ora vedrai 5 numeri, avrai a disposizione 30 secondi per memorizzarli, dopo di che dovrai inserire quelli che ti ricordi')
+
     //Funzione per generare un numero random
     function getRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,7 +26,11 @@ $(function() {
     $(numeriGenerati).text(numeriPcRandom);
 
     //Genero un countdown
-    var countdown = 5;
+    var countdown = 30;
+    
+    var userNumbers = [];
+
+    var indovinati = [];
     
     var interval = setInterval(function () {  
         
@@ -31,23 +38,30 @@ $(function() {
         
         //Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
         if (countdown === 0) {
-            
             $(numeriGenerati).hide();
             
-            var userNumbers = [];
-
             setTimeout(askNumber, 1000);
+
             function askNumber() {
                 for (var i = 0;i < 5; i++) {
                     var numeroUtente = Number(prompt('Inserisci uno a uno i numeri che hai visto'));
                     
                     userNumbers.push(numeroUtente);
-                };
 
+                    if(numeriPcRandom.includes(numeroUtente)) {
+                        indovinati.push(numeroUtente);
+                    }
+                    
+                };
                 console.log(userNumbers);
+                console.log(indovinati);
+                
+                //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+                $('#numeri_giusti').text('Hai indovinato ' + indovinati.length + ' numeri: ' + indovinati);
             }
             
             clearInterval(interval);
+        
         } else {
             countdown--;
         }
@@ -57,9 +71,3 @@ $(function() {
 
 });
 
-//Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
-
-
-function myFunction() {
-    alert('Hello');
-}
