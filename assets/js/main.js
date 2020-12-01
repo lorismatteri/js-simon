46d0629270fbae51af06c6798e5f7b89fd99ea10
Loris Matteri <loris.matteri93@gmail.com>
2020-12-01 15:44:55 +0100

    Generazione numeri random
    Inserita funzione per generare numero random, reiterata per 5 volte. con includes inserisco nell'array dei numeri random solo numeri diversi. con un alert li faccio vedere

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
 $(function() {
-    console.log('ciao');
+    //Funzione per generare un numero random
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //Genero 5 numeri random tra 1 e 100 e mi assicuro che siano diversi prima di inserirli nell'array
+    
+    var numeriPcRandom = [];
+    
+    while (numeriPcRandom.length !== 5) {
+        var number = getRandomNumber(1, 100);
+        if (! numeriPcRandom.includes(number)) {
+            numeriPcRandom.push(number);
+        }
+    }
+    
+    console.log(numeriPcRandom.sort());
+
+    //Un alert espone 5 numeri casuali diversi.
+    alert(numeriPcRandom.sort());
+    
+    //Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+    
 });
 
-//Un alert espone 5 numeri casuali diversi.
-//Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
